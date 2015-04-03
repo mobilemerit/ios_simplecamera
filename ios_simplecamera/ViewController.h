78655e1460140034765c212866c26c98609d508b
Pankaj Phartiyal <pankajphartiyal90@gmail.com>
2015-04-03 15:58:36 +0530

    basic camera app
    switch camera, save photo, access camera roll, toggle 3 flash modes

--- ios_simplecamera/ViewController.h
+++ ios_simplecamera/ViewController.h
@@ -7,9 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GPUImage/GPUImage.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    GPUImageStillCamera *stillCamera;
+    GPUImageFilter *filter;
+    NSArray *cameraFlashStates;
+    int activeState;
+    UIImagePickerController *imagePickerC;
+    BOOL cameraFront;
+}
 
+@property (strong, nonatomic) IBOutlet GPUImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIButton *flashButton;
+
+- (IBAction)toggleFlash:(id)sender;
+- (IBAction)capture:(id)sender;
+- (IBAction)goToCameraRoll:(id)sender;
+- (IBAction)switchCameraSource:(id)sender;
 
 @end
 
--- ios_simplecamera/ViewController.m
+++ ios_simplecamera/ViewController.m
@@ -17,6 +17,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    cameraFlashStates = @[@"auto", @"on", @"off"];
+    activeState = 0;
+    [self setAutoFlashMode];
+    
+    cameraFront = NO;
+    
+    imagePickerC = [[UIImagePickerController alloc] init];
+    [imagePickerC setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    
+    stillCamera = [[GPUImageStillCamera alloc] init];
+    stillCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    filter = [[GPUImageGammaFilter alloc] init];
+    [stillCamera addTarget:filter];
+    GPUImageView *filterView = _imageView;
+    [filter addTarget:filterView];
+    
+    [stillCamera startCameraCapture];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +42,71 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)setAutoFlashMode {
+    NSError *error = nil;
+    if (![stillCamera.inputCamera lockForConfiguration:&error])
+    {
+        NSLog(@"Error locking for configuration: %@", error);
+    }
+    [stillCamera.inputCamera setFlashMode:AVCaptureFlashModeAuto];
+    [stillCamera.inputCamera unlockForConfiguration];
+    [_flashButton setImage:[UIImage imageNamed:@"flashauto"] forState:UIControlStateNormal];
+}
+
+-(void)setOnFlashMode {
+    NSError *error = nil;
+    if (![stillCamera.inputCamera lockForConfiguration:&error])
+    {
+        NSLog(@"Error locking for configuration: %@", error);
+    }
+    [stillCamera.inputCamera setFlashMode:AVCaptureFlashModeOn];
+    [stillCamera.inputCamera unlockForConfiguration];
+    [_flashButton setImage:[UIImage imageNamed:@"flashon"] forState:UIControlStateNormal];
+}
+
+-(void)setOffFlashMode {
+    NSError *error = nil;
+    if (![stillCamera.inputCamera lockForConfiguration:&error])
+    {
+        NSLog(@"Error locking for configuration: %@", error);
+    }
+    [stillCamera.inputCamera setFlashMode:AVCaptureFlashModeOff];
+    [stillCamera.inputCamera unlockForConfiguration];
+    [_flashButton setImage:[UIImage imageNamed:@"flashoff"] forState:UIControlStateNormal];
+}
+
+- (IBAction)toggleFlash:(id)sender {
+    activeState = (activeState+1)%cameraFlashStates.count;
+    if ([cameraFlashStates[activeState] isEqualToString:@"auto"]) {
+        [self setAutoFlashMode];
+    } else if([cameraFlashStates[activeState] isEqualToString:@"on"]) {
+        [self setOnFlashMode];
+    } else if([cameraFlashStates[activeState] isEqualToString:@"off"]) {
+        [self setOffFlashMode];
+    }
+    
+}
+
+- (IBAction)capture:(id)sender {
+    [stillCamera capturePhotoAsImageProcessedUpToFilter:filter withCompletionHandler:^(UIImage *processedImage, NSError *error) {
+        NSData *dataForJPEGFile = UIImageJPEGRepresentation(processedImage, 0.8);
+
+        UIImageWriteToSavedPhotosAlbum([UIImage imageWithData:dataForJPEGFile], nil, nil, nil);
+        return ;
+    }];
+}
+
+- (IBAction)goToCameraRoll:(id)sender {
+    [self presentViewController:imagePickerC animated:YES completion:nil];
+}
+
+- (IBAction)switchCameraSource:(id)sender {
+    [stillCamera rotateCamera];
+    cameraFront = !cameraFront;
+    if (cameraFront) {
+        _flashButton.hidden = YES;
+    } else {
+        _flashButton.hidden = NO;
+    }
+}
 @end
