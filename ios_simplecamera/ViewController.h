444e412dd05d3440ac89919e575de24018b29829
Pankaj Phartiyal <pankajphartiyal90@gmail.com>
2015-04-03 13:22:26 +0530

    Initial Commit

---
+++ ios_simplecamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_simplecamera
+//
+//  Created by Pankaj on 03/04/15.
+//  Copyright (c) 2015 Mobilemerit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios_simplecamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios_simplecamera
+//
+//  Created by Pankaj on 03/04/15.
+//  Copyright (c) 2015 Mobilemerit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
